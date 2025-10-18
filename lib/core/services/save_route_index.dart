51f88b93830bf766213618802dfa20635a6a230c
Ahmed Adel <ahmedadelelmelegy2@gmail.com>
2025-10-18 15:53:32 +0300

    update local storage to settion storage

--- lib/core/services/save_route_index.dart
+++ lib/core/services/save_route_index.dart
@@ -2,10 +2,10 @@ import 'package:customs/core/utils/app_constants.dart';
 import 'package:universal_html/html.dart' as html;
 
 void saveRouteIndex(int index) {
-  html.window.localStorage[AppConstants.routeIndexKey] = index.toString();
+  html.window.sessionStorage[AppConstants.routeIndexKey] = index.toString();
 }
 
 int? getRouteIndex() {
-  final value = html.window.localStorage[AppConstants.routeIndexKey];
-  return value != null ? int.tryParse(value) : null;
+  final value = html.window.sessionStorage[AppConstants.routeIndexKey];
+  return value != null ? int.parse(value) : null;
 }
